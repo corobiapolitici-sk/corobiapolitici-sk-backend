d6962854b31e922a4ad376b8bbb3042a2d95bcbe
Osama Hassanein <osama.e.hassanein@gmail.com>
2019-05-27 18:20:43 +0200

    Add vybor generating code

---
+++ index.js
@@ -0,0 +1,49 @@
+// Load local modules.
+const vyborScript = require('./script/vybor')
+
+// Load npm modules.
+const { MongoClient } = require('mongodb')
+
+const url = 'mongodb://localhost:27017'
+
+/*
+edges_hlasovanie_zakon_hlasovalo_o
+edges_hlasovanie_zmena_hlasovalo_o
+edges_klub_spektrum_clen
+edges_poslanec_delegacia_clen
+edges_poslanec_hlasovanie_hlasoval
+edges_poslanec_klub_bol_clenom
+edges_poslanec_klub_clen
+edges_poslanec_rozprava_vystupil
+// edges_poslanec_vybor_clen
+edges_poslanec_zakon_navrhol
+edges_poslanec_zmena_navrhol
+edges_poslanec_zmena_podpisal
+edges_rozprava_zakon_tykala_sa
+edges_spektrum_zakon_navrhol
+// edges_vybor_zakon_gestorsky
+// edges_vybor_zakon_navrhnuty
+edges_zmena_zakon_navrhnuta
+nodes_delegacia
+nodes_hlasovanie
+nodes_klub
+nodes_poslanec
+nodes_rozprava
+nodes_spektrum
+nodes_zakon
+nodes_zmena
+*/
+
+;(async () => {
+	const client = await MongoClient.connect(url, { useNewUrlParser: true })
+
+	try {
+		const db = client.db('corobiapolitici')
+
+		await vyborScript(db)
+	} catch (exception) {
+		throw exception
+	} finally {
+		await client.close()
+	}
+})()
---
+++ script/vybor.js
@@ -0,0 +1,46 @@
+// Load npm modules.
+const fsExtra = require('fs-extra')
+
+// Load node modules.
+const path = require('path')
+
+const dataMapFilePath = path.join(__dirname, 'vybor.json')
+const imageDirectoryPath = path.join(__dirname, '..', 'image', 'vybor')
+
+/*
+nodes_vybor
+edges_vybor_zakon_gestorsky
+edges_vybor_zakon_navrhnuty
+edges_poslanec_vybor_clen
+*/
+
+module.exports = async (db) => {
+	const nodes = await db.collection('nodes_vybor').find().toArray()
+
+	const dataMap = new Map([...Object.entries(await fsExtra.readJson(dataMapFilePath))])
+
+	for (const node of nodes) {
+		node._id = node._id.toString()
+		node.name = node.id
+		node.tag = ''
+		node.imageUrl = '/preprocessed/image/vybor/default.png'
+
+		if (dataMap.has(node.id)) {
+			const {
+				name,
+				tag,
+			} = dataMap.get(node.id)
+
+			node.name = name
+			node.tag = tag
+
+			if (await fsExtra.exists(path.join(imageDirectoryPath, `${tag}.png`))) {
+				node.imageUrl = `/preprocessed/image/vybor/${tag}.png`
+			}
+		}
+	}
+
+	await fsExtra.writeJson(path.join(__dirname, '..', 'data', 'vybor.json'), nodes)
+
+	return nodes
+}
